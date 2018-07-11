bb8dc982b5b1e6266938a9a26bf45ba05442bae9
DanielyanAndranik <danielyandranik@gmail.com>
2018-07-11 16:24:20 +0400

    Adding stored procedures

---
+++ InstitutionDB/dbo/Stored Procedures/uspCreateInstitution.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[uspCreateInstitution]
+	@name nvarchar(50),
+	@type nvarchar(15),
+	@email varchar(320),
+	@description text,
+	@country nvarchar(20),
+	@state nvarchar(20),
+	@city nvarchar(20),
+	@postalCode nchar(10),
+	@addressLine nvarchar(100)
+AS
+	begin
+		
+	end
