6f963e36c73b0d073bff28b3e754bdfaf7a602b9
DanielyanAndranik <danielyandranik@gmail.com>
2018-07-11 21:22:14 +0400

    Adding Recipe Api

---
+++ RecipeApi/Context/IRecipeContext.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+using RecipeApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeApi.Context
+{
+    interface IRecipeContext
+    {
+        IMongoCollection<Recipe> Recipes { get; }
+    }
+}
---
+++ RecipeApi/Context/RecipeContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using RecipeApi.Models;
+
+namespace RecipeApi.Context
+{
+    public class RecipeContext : IRecipeContext
+    {
+        private readonly IMongoDatabase _db;
+
+        public RecipeContext(IOptions<Settings.Settings> options)
+        {
+            var client = new MongoClient(options.Value.ConnectionString);
+            _db = client.GetDatabase(options.Value.Database);
+        }
+
+        public IMongoCollection<Recipe> Recipes => _db.GetCollection<Recipe>("Recipes");
+    }
+}
