9349772d3db41fd9748ee73164383870fbd4755f
babujyan <31545427+babujyan@users.noreply.github.com>
2018-07-13 18:12:20 +0400

    the lastest one for today

--- MedecineAPI/Models/Medicine.cs
+++ MedecineAPI/Models/Medicine.cs
@@ -9,11 +9,44 @@ namespace MedicineAPI.Models
 {
     public class Medicine
     {
+        /// <summary>
+        /// Medicine ID.
+        /// </summary>
 		[BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
 		public string Id { get; set; }
+
+        /// <summary>
+        /// Name of the Medicine.
+        /// </summary>
 		public string Name { get; set; }
+
+        /// <summary>
+        /// Who made this product(Company name).
+        /// </summary>
         public string Maker { get; set; }
+
+        /// <summary>
+        /// Country where it was made.
+        /// </summary>
         public string Country { get; set; }
-	}
+
+        /// <summary>
+        /// Units how it counts.
+        /// </summary>
+        public string Units { get; set; }
+
+        /// <summary>
+        /// Shelf life by mothes.
+        /// </summary>
+        public int ShelfLife { get; set;}
+
+
+        /// <summary>
+        /// Information about Medicine.
+        /// </summary>
+        public string Description { get; set; }
+
+
+    }
 }
