0553c200707a5489db4288eb1441439c88d61de8
babujyan <31545427+babujyan@users.noreply.github.com>
2018-07-13 12:25:43 +0400

    rebase

---
+++ MedecineAPI/Context/MedicineContex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MedicineAPI.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace MedicineAPI.Context
+{
+	public class MedicineContex : IMedicineConext
+	{
+		private readonly IMongoDatabase _db;
+				
+		public MedicineContex(IOptions<Settings.Settings> options)
+		{
+			var client = new MongoClient(options.Value.ConnectionString);
+			_db = client.GetDatabase(options.Value.Database);
+		}
+
+		IMongoCollection<Medicine> IMedicineConext.Medicines => _db.GetCollection<Medicine>("Medicine");
+	}
+}
---
+++ MedecineAPI/Models/Medicine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MedicineAPI.Models
+{
+    public class Medicine
+    {
+		[BsonId]
+		public ObjectId Id { get; set; }
+		public string Name { get; set; }
+		public string Developer { get; set; }
+		public string Publisher { get; set; }
+		public List<string> Platforms { get; set; }
+	}
+}
