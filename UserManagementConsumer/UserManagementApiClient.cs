84585a62eb825463adcec3a12431b77885ee847d
amirkhaniansev <35461426+amirkhaniansev@users.noreply.github.com>
2018-07-17 21:19:09 +0400

    added User Management API client

---
+++ UserManagementConsumer/UserManagementApiClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserManagementConsumer
+{
+    /// <summary>
+    /// Class for consuming User Management API
+    /// </summary>
+    public class UserManagementApiClient
+    {
+
+    }
+}
