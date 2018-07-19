e4a836e7ea8ebae486827280f8c630cb153f849f
DanielyanAndranik <danielyandranik@gmail.com>
2018-07-20 00:46:14 +0400

    Upgrading UI

--- Desktop/Views/Pages/Medicines.xaml.cs
+++ Desktop/Views/Pages/Medicines.xaml.cs
@@ -23,6 +23,207 @@ namespace Desktop.Views.Pages
         public Medicines()
         {
             InitializeComponent();
+            this.Tweets.ItemsSource = new[] 
+            {
+                new Medicine
+                {
+                    Id = "Id_1",
+                    Name = "Ascofen",
+                    Maker = "John LLC",
+                    Country = "Armenia",
+                    Units = "Miligram",
+                    ShelfLife = 15,
+                    Description = "Empty"
+                },
+                new Medicine
+                {
+                    Id = "Id_1",
+                    Name = "Ascofen",
+                    Maker = "John LLC",
+                    Country = "Armenia",
+                    Units = "Miligram",
+                    ShelfLife = 15,
+                    Description = "Empty"
+                },
+                new Medicine
+                {
+                    Id = "Id_1",
+                    Name = "Ascofen",
+                    Maker = "John LLC",
+                    Country = "Armenia",
+                    Units = "Miligram",
+                    ShelfLife = 15,
+                    Description = "Empty"
+                },
+                new Medicine
+                {
+                    Id = "Id_1",
+                    Name = "Ascofen",
+                    Maker = "John LLC",
+                    Country = "Armenia",
+                    Units = "Miligram",
+                    ShelfLife = 15,
+                    Description = "Empty"
+                },
+                new Medicine
+                {
+                    Id = "Id_1",
+                    Name = "Ascofen",
+                    Maker = "John LLC",
+                    Country = "Armenia",
+                    Units = "Miligram",
+                    ShelfLife = 15,
+                    Description = "Empty"
+                },
+                new Medicine
+                {
+                    Id = "Id_1",
+                    Name = "Ascofen",
+                    Maker = "John LLC",
+                    Country = "Armenia",
+                    Units = "Miligram",
+                    ShelfLife = 15,
+                    Description = "Empty"
+                },
+                new Medicine
+                {
+                    Id = "Id_1",
+                    Name = "Ascofen",
+                    Maker = "John LLC",
+                    Country = "Armenia",
+                    Units = "Miligram",
+                    ShelfLife = 15,
+                    Description = "Empty"
+                },
+                new Medicine
+                {
+                    Id = "Id_1",
+                    Name = "Ascofen",
+                    Maker = "John LLC",
+                    Country = "Armenia",
+                    Units = "Miligram",
+                    ShelfLife = 15,
+                    Description = "Empty"
+                },
+                new Medicine
+                {
+                    Id = "Id_1",
+                    Name = "Ascofen",
+                    Maker = "John LLC",
+                    Country = "Armenia",
+                    Units = "Miligram",
+                    ShelfLife = 15,
+                    Description = "Empty"
+                },
+                new Medicine
+                {
+                    Id = "Id_1",
+                    Name = "Ascofen",
+                    Maker = "John LLC",
+                    Country = "Armenia",
+                    Units = "Miligram",
+                    ShelfLife = 15,
+                    Description = "Empty"
+                },
+                new Medicine
+                {
+                    Id = "Id_1",
+                    Name = "Ascofen",
+                    Maker = "John LLC",
+                    Country = "Armenia",
+                    Units = "Miligram",
+                    ShelfLife = 15,
+                    Description = "Empty"
+                },
+                new Medicine
+                {
+                    Id = "Id_1",
+                    Name = "Ascofen",
+                    Maker = "John LLC",
+                    Country = "Armenia",
+                    Units = "Miligram",
+                    ShelfLife = 15,
+                    Description = "Empty"
+                },
+                new Medicine
+                {
+                    Id = "Id_1",
+                    Name = "Ascofen",
+                    Maker = "John LLC",
+                    Country = "Armenia",
+                    Units = "Miligram",
+                    ShelfLife = 15,
+                    Description = "Empty"
+                },
+                new Medicine
+                {
+                    Id = "Id_1",
+                    Name = "Ascofen",
+                    Maker = "John LLC",
+                    Country = "Armenia",
+                    Units = "Miligram",
+                    ShelfLife = 15,
+                    Description = "Empty"
+                },
+                new Medicine
+                {
+                    Id = "Id_1",
+                    Name = "Ascofen",
+                    Maker = "John LLC",
+                    Country = "Armenia",
+                    Units = "Miligram",
+                    ShelfLife = 15,
+                    Description = "Empty"
+                },
+                new Medicine
+                {
+                    Id = "Id_1",
+                    Name = "Ascofen",
+                    Maker = "John LLC",
+                    Country = "Armenia",
+                    Units = "Miligram",
+                    ShelfLife = 15,
+                    Description = "Empty"
+                }
+            };
         }
+    }
+
+    public class Medicine
+    {
+        /// <summary>
+        /// Medicine ID.
+        /// </summary>
+        public string Id { get; set; } = null;
+
+        /// <summary>
+        /// Name of the Medicine.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Who made this product(Company name).
+        /// </summary>
+        public string Maker { get; set; }
+
+        /// <summary>
+        /// Country where it was made.
+        /// </summary>
+        public string Country { get; set; }
+
+        /// <summary>
+        /// Units how it counts.
+        /// </summary>
+        public string Units { get; set; }
+
+        /// <summary>
+        /// Shelf life by mothes.
+        /// </summary>
+        public int ShelfLife { get; set; }
+
+        /// <summary>
+        /// Information about Medicine.
+        /// </summary>
+        public string Description { get; set; }
     }
 }
