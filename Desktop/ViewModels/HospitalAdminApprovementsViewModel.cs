bbbf2c0add7b584343dfe1d9af074455cca0c290
DanielyanAndranik <danielyandranik@gmail.com>
2018-07-30 17:49:44 +0400

    Adding HospitalAdminApprovementsViewModel

---
+++ Desktop/ViewModels/HospitalAdminApprovementsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+
+namespace Desktop.ViewModels
+{
+    class HospitalAdminApprovementsViewModel : ViewModelBase
+    {
+        private ObservableCollection<DoctorInfo> waitingCollection;
+
+        public ObservableCollection<DoctorInfo> WaitingCollection
+        {
+            get => this.waitingCollection;
+            set => this.Set("WaitingCollection", ref this.waitingCollection, value);
+        }
+    }
+}
