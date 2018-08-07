b6f45f532d1368d7defe460d981e9eac362b969e
amirkhaniansev <35461426+amirkhaniansev@users.noreply.github.com>
2018-08-08 01:18:01 +0400

    added NavigateCommand

---
+++ Desktop/Commands/NavigateCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+using System.Windows.Controls;
+
+namespace Desktop.Commands
+{
+    public class NavigateCommand<T> : ICommand
+        where T : Page, new()
+    {
+        private readonly Frame _frame;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+
+        public NavigateCommand(Frame frame)
+        {
+            this._frame = frame;  
+        }
+
+        public void Execute(object parameter)
+        {
+            var page = new T();
+
+            this._frame.NavigationService.Navigate(page);
+        }
+    }
+}
