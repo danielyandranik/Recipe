df7a4a10791957e3291ee287da1aeabfc48d203f
amirkhaniansev <35461426+amirkhaniansev@users.noreply.github.com>
2018-08-09 02:32:30 +0400

    added some changes

--- Desktop/Views/Pages/MapPage.xaml.cs
+++ Desktop/Views/Pages/MapPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Desktop.ViewModels;
+using Microsoft.Maps.MapControl.WPF.Core;
 
 namespace Desktop.Views.Pages
 {
@@ -26,6 +28,7 @@ namespace Desktop.Views.Pages
         public MapPage()
         {
             InitializeComponent();
+
             this._vm = new MapPageViewModel(this.map.Children);
             this.DataContext = this._vm;
         }
