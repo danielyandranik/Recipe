53926fd144444a61b7c792c0ea28ddaeb9f1e16c
amirkhaniansev <35461426+amirkhaniansev@users.noreply.github.com>
2018-08-11 06:34:01 +0400

    added some changes to Recipe API

---
+++ RecipeApi/Controllers/RecipeQrCodesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RecipeApi.Services;
+
+namespace RecipeApi.Controllers
+{
+    [Authorize(Policy = "CanWorkWithRecipe")]
+    [Produces("application/jsosn")]
+    [Route("api/recipe-qr-codes")]
+    public class RecipeQrCodesController : Controller
+    {
+        private readonly QrMailSender _qrMailSender;
+
+        private readonly QrCodeService _qrCodeService;
+
+        public RecipeQrCodesController(QrMailSender qrMailSender,QrCodeService qrCodeService)
+        {
+            this._qrMailSender = qrMailSender;
+            this._qrCodeService = qrCodeService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]QrSendInfo qrInfo)
+        {
+            if (qrInfo == null)
+                return new StatusCodeResult(404);
+
+            var image = await this._qrCodeService.GetQrImage(qrInfo.RecipeId);
+
+            if (image == null)
+                return new StatusCodeResult(404);
+
+            await this._qrMailSender.SendAsync(qrInfo.Email, qrInfo.RecipeId, image);
+
+            return Ok();
+        }
+    }
+}
---
+++ RecipeApi/Services/QrMailSender.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace RecipeApi.Services
+{
+    public class QrMailSender
+    {
+        private readonly SmtpClient _smptpClient;
+
+        private readonly NetworkCredential _networkCredential;
+
+        public QrMailSender(NetworkCredential networkCredential)
+        {
+            this._networkCredential = networkCredential;
+
+            this._smptpClient = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                Credentials = networkCredential,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network
+            };
+        }
+        
+        public async Task SendAsync(string to,string recipeId,Bitmap qrImage)
+        {
+            await this.GetSendTask(to,recipeId,qrImage);
+        }
+
+        private Task GetSendTask(string to,string recipeId,Bitmap qrImage)
+        {
+            var task = new Task(() =>
+            {
+                var mail = new MailMessage
+                {
+                    From = new MailAddress(this._networkCredential.UserName),
+                    Subject = "Recipe QR code",
+                    IsBodyHtml = true
+                };
+
+                var stream = new MemoryStream();
+
+                qrImage.Save(stream, ImageFormat.Bmp);
+
+                var img = new LinkedResource(stream, "image/bmp")
+                {
+                    ContentId = "recipe_qr_code"
+                };
+
+                var foot = AlternateView.CreateAlternateViewFromString("<p> <img src=cid:recipe_qr_code /> </p>", null, "text/html");
+
+                foot.LinkedResources.Add(img);
+
+                mail.AlternateViews.Add(foot);
+
+                mail.To.Add(to);
+
+                this._smptpClient.Send(mail);
+            });
+
+            task.Start();
+
+            return task;
+        }
+    }
+}
