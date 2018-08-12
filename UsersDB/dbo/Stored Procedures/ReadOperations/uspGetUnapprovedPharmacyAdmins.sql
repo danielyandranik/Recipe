9ed89fa31bad635ae784ee8c2d400899f9ab03a2
amirkhaniansev <35461426+amirkhaniansev@users.noreply.github.com>
2018-08-13 00:28:41 +0400

    fixed UsersDB

--- UsersDB/dbo/Stored Procedures/ReadOperations/uspGetUnapprovedPharmacyAdmins.sql
+++ UsersDB/dbo/Stored Procedures/ReadOperations/uspGetUnapprovedPharmacyAdmins.sql
@@ -1,9 +1,10 @@
 ﻿CREATE PROCEDURE [dbo].[uspGetUnapprovedPharmacyAdmins]
 AS
-	select p.UserId, u.FullName, a.PharmacyName, p.CreatedDate, a.StartedWorking 
-	from Users u
-	join UserProfile p on u.Id = p.UserId
-	join PharmacyAdmins a on p.ProfileId = a.ProfileId
-	where p.[Type] = 'pharmacy_admin' and p.IsApproved = 0
-
+	begin
+		select p.UserId, u.FullName, a.PharmacyName, p.CreatedDate, a.StartedWorking 
+			from Users u
+				join UserProfile p on u.Id = p.UserId
+				join PharmacyAdmins a on p.ProfileId = a.ProfileId
+				where p.[Type] = 'pharmacy_admin' and p.IsApproved = 0
+	end
 RETURN 0
--- UsersDB/dbo/Stored Procedures/ReadOperations/uspGetUnapprvedHospitalAdmins.sql
+++ UsersDB/dbo/Stored Procedures/ReadOperations/uspGetUnapprvedHospitalAdmins.sql
@@ -1,9 +1,10 @@
 ﻿CREATE PROCEDURE [dbo].[uspGetUnapprvedHospitalAdmins]
 AS
-	select p.UserId, u.FullName, h.HospitalName, p.CreatedDate, h.StartedWorking 
-	from Users u
-	join UserProfile p on u.Id = p.UserId
-	join HospitalDirectors h on p.ProfileId = h.ProfileId
-	where p.[Type] = 'hospital_director' and p.IsApproved = 0
-
+	begin
+		select p.UserId, u.FullName, h.HospitalName, p.CreatedDate, h.StartedWorking 
+			from Users u
+				join UserProfile p on u.Id = p.UserId
+				join HospitalDirectors h on p.ProfileId = h.ProfileId
+				where p.[Type] = 'hospital_director' and p.IsApproved = 0
+	end
 RETURN 0
