9d022ef7fffb168698675c9f703082ba90102502
Hakobyan Sona <hakobyansona.98@gmail.com>
2018-08-12 08:02:52 -0700

    Added medicine filter

---
+++ Desktop/Services/LoadSuppliersService.cs
@@ -0,0 +1,57 @@
+﻿using Desktop.Interfaces;
+using Desktop.ViewModels;
+using MedicineApiClient;
+using InstitutionClient.Models;
+using System;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Desktop.Services
+{
+    public class LoadSuppliersService : ILoadService
+    {
+        private PharmaciesViewModel pharmaciesViewModel;
+
+        private readonly InstitutionClient.Client instClient;
+
+        private readonly Client medClient;
+
+        public string MedicineName;
+
+        public LoadSuppliersService(PharmaciesViewModel pharmaciesViewModel)
+        {
+            this.pharmaciesViewModel = pharmaciesViewModel;
+            this.instClient = ((App)App.Current).InstitutionClient;
+            this.medClient = ((App)App.Current).MedicineClient;
+        }
+
+        public async Task Load()
+        {
+            var medicineId = await this.GetMedicineId();
+            
+            var response = await this.instClient.GetAllSuppliersAsync(medicineId);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception();
+            }
+
+            this.pharmaciesViewModel.Pharmacies = new ObservableCollection<Institution>(response.Content);
+        }
+
+
+        private async Task<int> GetMedicineId()
+        {
+            var response = await this.medClient.GetMedicineAsync($"api/medicines/?medicineName={this.MedicineName}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception();
+            }
+
+            int.TryParse(response.Result.Id, out int id);
+
+            return id;
+        }
+    }
+}
--- Desktop/Views/Pages/Pharmacies.xaml.cs
+++ Desktop/Views/Pages/Pharmacies.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using Desktop.Services;
 using Desktop.ViewModels;
@@ -12,61 +11,99 @@ namespace Desktop.Views.Pages
     /// </summary>
     public partial class Pharmacies : Page
     {
+        /// <summary>
+        /// Pharmacies view model
+        /// </summary>
         public PharmaciesViewModel PharmaciesViewModel { get; private set; }
 
-        private LoadPharmMedicinesService service;
+        /// <summary>
+        /// Service for loading pharmacy medicines
+        /// </summary>
+        public LoadPharmMedicinesService MedicinesService { get; private set; }
 
+        /// <summary>
+        /// Service for loading suppliers of given medicine
+        /// </summary>
+        public LoadSuppliersService SuppliersService { get; private set; }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Pharmacies"/>
+        /// </summary>
         public Pharmacies()
         {
             InitializeComponent();
 
             this.PharmaciesViewModel = new PharmaciesViewModel();
             this.DataContext = this.PharmaciesViewModel;
-
-            this.service = new LoadPharmMedicinesService(this.PharmaciesViewModel);
+            this.MedicinesService = new LoadPharmMedicinesService(this.PharmaciesViewModel);
+            this.SuppliersService = new LoadSuppliersService(this.PharmaciesViewModel);
         }
 
+        /// <summary>
+        /// Click for editing pharmacy
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">The Routed Event Args</param>
         private void EditPharmacyClick(object sender, RoutedEventArgs e)
         {
             var id = (int)(sender as Button).Tag;
             this.PharmaciesViewModel.EditablePharmacy = new Institution() { Id = id };
+
+            // Open popup and disable Main Window
             this.EditPopup.IsOpen = true;
             Application.Current.MainWindow.IsEnabled = false;
         }
 
+        /// <summary>
+        /// Click for viewing medicines
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">The Routed Event Args</param>
         private void ViewMedicinesClick(object sender, RoutedEventArgs e)
         {
             var id = (int)(sender as Button).Tag;
+            this.MedicinesService.CurrentPharamcy = id;
+            var response =  this.MedicinesService.Load();
 
-            this.service.id = id;
-            Task response =  this.service.Load();
-
+            // Open popup and disable Main Window
             this.MedicinesPopup.IsOpen = true;
             Application.Current.MainWindow.IsEnabled = false;
         }
 
+        /// <summary>
+        /// Click for closing Edit pharmacy popup
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">The Routed Event Args</param>
         private void CloseEditPharmacyPopup_Click(object sender, RoutedEventArgs e)
         {
             this.EditPopup.IsOpen = false;
             Application.Current.MainWindow.IsEnabled = true;
         }
 
+        /// <summary>
+        /// Click for closing View medicines popup
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">The Routed Event Args</param>
         private void CloseViewMedicinesPopup_Click(object sender, RoutedEventArgs e)
         {
             this.MedicinesPopup.IsOpen = false;
             Application.Current.MainWindow.IsEnabled = true;
         }
-        
 
+        /// <summary>
+        /// Pharmacies filter event
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">The Routed Event Args</param>
         private async void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            // Getting the textbox
             var textbox = sender as TextBox;
-
-            if (textbox == this.medicine)
-            {
-               // await this.PharmaciesViewModel.Filter(pharmacy => pharmacy.Medicine.Contains(textbox.Text));
-            }
-            else if (textbox == this.name)
+            
+            // Filter pharmacies by appropriate predicate
+            if (textbox == this.name)
             {
                 await this.PharmaciesViewModel.Filter(pharmacy => pharmacy.Name.Contains(textbox.Text));
             }
@@ -74,6 +111,17 @@ namespace Desktop.Views.Pages
             {
                 await this.PharmaciesViewModel.Filter(pharmacy => pharmacy.Address.Contains(textbox.Text));
             }
+        }
+
+        private async void TextBox_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key != System.Windows.Input.Key.Enter) return;
+
+            // Getting the textbox
+            var textbox = sender as TextBox;
+
+            this.SuppliersService.MedicineName = textbox.Text;
+            await this.SuppliersService.Load();
         }
     }
 }
