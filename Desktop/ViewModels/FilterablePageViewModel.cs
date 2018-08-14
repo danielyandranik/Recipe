90f8a8d38de6dbb1d556714704611ff1db20ff04
amirkhaniansev <35461426+amirkhaniansev@users.noreply.github.com>
2018-08-14 17:00:49 +0400

    moved filtering into FilterCommand in HospitalsPage

---
+++ Desktop/ViewModels/FilterablePageViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+
+namespace Desktop.ViewModels
+{
+    /// <summary>
+    /// View model for pages that are loadable and have filterable content
+    /// </summary>
+    public abstract class FilterablePageViewModel : LoadablePageViewModel
+    {
+        /// <summary>
+        /// Name filter text
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Address filter text
+        /// </summary>
+        private string _address;
+
+        /// <summary>
+        /// Gets or sets name
+        /// </summary>
+        public string Name
+        {
+            // gets name
+            get => this._name;
+
+            // sets name
+            set => this.Set("Name", ref this._name, value);
+        }
+
+        /// <summary>
+        /// Gets or sets address
+        /// </summary>
+        public string Address
+        {
+            // gets address
+            get => this._address;
+
+            // sets address
+            set => this.Set("Address", ref this._address, value);
+        }
+
+        /// <summary>
+        /// Filters content
+        /// </summary>
+        /// <returns>nothing</returns>
+        public abstract Task Filter();
+    }
+}
